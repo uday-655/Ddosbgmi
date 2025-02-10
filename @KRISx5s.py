fe71f2e11c389b8880414ba329fd746fb57c0b49
uday-655 <jiwaj5158@gmail.com>
2025-02-10 23:21:24 +0530

    Update @KRISx5s.py

--- @KRISx5s.py
+++ @KRISx5s.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ''
+TOKEN = '7743425789:AAHv3U-QhDB4KX0JCFVoydBANaFOdz39k38'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
-FORWARD_CHANNEL_ID = -1002156421934
-CHANNEL_ID = -1002156421934
-error_channel_id = -1002156421934
+FORWARD_CHANNEL_ID = -4708834178
+CHANNEL_ID = -4708834178
+error_channel_id = -4708834178
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
